5a3236ecd8167e095f9612c771ab69843727680d
soulbreaker156 <soulbreaker0220@gmail.com>
2025-10-02 09:50:49 -0600

    feat: creado los seeders de roles y usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
+        $this->call([
+            RolSeeder::class,
+            UsuarioSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Usuario;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Usuario::create([
+            'usuario' => 'admin',
+            'contrasena' => Hash::make('admin123'),
+            'nit' => '123456789',
+            'direccion' => 'Calle Falsa 123',
+            'fk_id_rol' => 1,
+        ]);
+    }
+}

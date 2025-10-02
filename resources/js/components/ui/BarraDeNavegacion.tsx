4ad57696bd6cf8498e2a973198b70a277c2a37fb
soulbreaker156 <soulbreaker0220@gmail.com>
2025-10-02 16:35:19 -0600

    feat: Agregado iconos en la barra de navegacion

--- resources/js/components/ui/BarraDeNavegacion.tsx
+++ resources/js/components/ui/BarraDeNavegacion.tsx
@@ -2,35 +2,92 @@ export default function BarraDeNavegacion() {
     return (
         <>
             <div className="flex w-1/6 flex-col items-center bg-[#736D6D] p-4">
-                <div className="bg-white rounded-full mb-6 w-32 h-32 overflow-hidden">
-                    <img className="object-cover" src="/images/KaijuKing.jpeg" alt="Profile"/>
+                <div className="mb-6 h-32 w-32 overflow-hidden rounded-full bg-white">
+                    <img
+                        className="object-cover"
+                        src="/images/KaijuKing.jpeg"
+                        alt="Profile"
+                    />
                 </div>
-
-                <nav className="w-full text-white text-2xl">
-                    <ul>
-                        <li>
-                            <a href="/dashboard">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#"></a>Catalogo
-                        </li>
-                        <li>
-                            <a href="#"></a>Facturación
-                        </li>
-                        <li>
-                            <a href="#"></a>Creditos
-                        </li>
-                        <li>
-                            <a href="#"></a>Inventario
-                        </li>
-                        <li>
-                            <a href="#"></a>Pedidos
-                        </li>
-                        <li>
-                            <a href="#"></a>Logout
-                        </li>
-                    </ul>
-                </nav>
+                <div className="flex w-full flex-col items-center">
+                    <nav className="w-full text-3xl text-white">
+                        <ul>
+                            <li className="mb-6">
+                                <a
+                                    href="/dashboard"
+                                    className="flex items-center gap-2"
+                                >
+                                    <img
+                                        src="/assets/icon/dashboard.png"
+                                        alt="Dashboard"
+                                        className="h-6 w-6"
+                                    />
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/catalogo.png"
+                                        alt="Catalogo"
+                                        className="h-6 w-6"
+                                    />
+                                    Catalogo
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/facturacion.png"
+                                        alt="Facturación"
+                                        className="h-6 w-6"
+                                    />
+                                    Facturación
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/credito.png"
+                                        alt="Creditos"
+                                        className="h-6 w-6"
+                                    />
+                                    Creditos
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/inventario.png"
+                                        alt="Inventario"
+                                        className="h-6 w-6"
+                                    />
+                                    Inventario
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/pedido.png"
+                                        alt="Pedidos"
+                                        className="h-6 w-6"
+                                    />
+                                    Pedidos
+                                </a>
+                            </li>
+                            <li className="mb-6">
+                                <a href="#" className="flex items-center gap-2">
+                                    <img
+                                        src="/assets/icon/logout.png"
+                                        alt="Logout"
+                                        className="h-6 w-6"
+                                    />
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </>
     );

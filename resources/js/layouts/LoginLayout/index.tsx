3952a201d1e51d7f187be87a667e79260f9e8900
soulbreaker156 <soulbreaker0220@gmail.com>
2025-10-02 13:53:25 -0600

    feat: se agrego la imagen del logotipo, style:se agrego el responsive del login (aun se esta terminando)

--- resources/js/layouts/LoginLayout/index.tsx
+++ resources/js/layouts/LoginLayout/index.tsx
@@ -5,10 +5,15 @@ export default function LoginLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex h-screen items-center justify-center bg-white">
-            <div className="flex flex-col h-[50vh] min-w-[30%] items-center rounded-[10px] border-1 p-6">
-                <div className='flex flex-col min-w-full min-h-full'>{children}</div>
+        <div className="flex h-screen items-center justify-center gap-3 bg-white p-5">
+            <div className="flex min-h-[35%] min-w-[90%] flex-col items-center justify-center rounded-[10px] border border-gray-300 bg-white p-6 shadow-[-20px_15px_10px_0px_rgba(0,0,0,0.2)] sm:h-[50vh] sm:min-w-[30%] sm:justify-normal">
+                <div className="min-h-full min-w-full">{children}</div>
             </div>
+            <img
+                src="/images/Logotipo.jpg"
+                alt="Logotipo"
+                className="hidden rotate-90 sm:block md:block"
+            />
         </div>
     );
 }
--- resources/js/pages/Login/Login.tsx
+++ resources/js/pages/Login/Login.tsx
@@ -1,11 +1,11 @@
 import LoginLayout from '@/layouts/LoginLayout';
 import { useForm, usePage } from '@inertiajs/react';
-import {Inertia} from '@inertiajs/inertia';
 import React, { useEffect } from 'react';
 import Swal from 'sweetalert2';
 export default function LoginPage() {
-
-    const { flash } = usePage().props as { flash?: { title: string; message: string; icon: 'success' | 'error'}};
+    const { flash } = usePage().props as {
+        flash?: { title: string; message: string; icon: 'success' | 'error' };
+    };
     const { data, setData, post } = useForm({
         usuario: '',
         password: '',
@@ -17,44 +17,59 @@ export default function LoginPage() {
     };
 
     useEffect(() => {
-      if (flash?.icon === 'success') {
-        Swal.fire({
-          title: flash.title,
-          text: flash.message,
-          icon: flash.icon,
-          timer: 1500,
-          showConfirmButton: false
-        });
-        // Puedes redirigir aquí si lo necesitas
-      } else if (flash?.icon === 'error') {
-        Swal.fire({
-          title: flash.title,
-          text: flash.message,
-          icon: flash.icon,
-          timer: 2500,
-          showConfirmButton: false
-        });
-      }
+        if (flash?.icon === 'success') {
+            Swal.fire({
+                title: flash.title,
+                text: flash.message,
+                icon: flash.icon,
+                timer: 1500,
+                showConfirmButton: false,
+            });
+        } else if (flash?.icon === 'error') {
+            Swal.fire({
+                title: flash.title,
+                text: flash.message,
+                icon: flash.icon,
+                timer: 2500,
+                showConfirmButton: false,
+            });
+        }
     }, [flash]);
 
     return (
         <LoginLayout>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="usuario">Usuario</label>
+            <form
+                className="mt-2 flex flex-col sm:mt-10"
+                onSubmit={handleSubmit}
+            >
+                <label className="text-[22px]" htmlFor="usuario">
+                    Usuario
+                </label>
                 <input
                     type="text"
                     id="usuario"
+                    className="mb-4 rounded-[8px] border border-gray-300 bg-[#DADADA] p-3 outline-none focus:border-gray-400"
                     value={data.usuario}
                     onChange={(e) => setData('usuario', e.target.value)}
                 />
-                <label htmlFor="password">Contraseña</label>
+                <label className="text-[22px]" htmlFor="password">
+                    Contraseña
+                </label>
                 <input
                     type="password"
                     id="password"
+                    className="mb-5 rounded-[8px] border border-gray-300 bg-[#DADADA] p-3 outline-none focus:border-gray-400"
                     value={data.password}
                     onChange={(e) => setData('password', e.target.value)}
                 />
-                <button type="submit">Iniciar Sesión</button>
+                <div className="flex min-w-full justify-center">
+                    <button
+                        className="min-w-[60%] rounded-[8px] bg-[#0B0B0B] p-2 text-[22px] text-white transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-[#282727]"
+                        type="submit"
+                    >
+                        Iniciar Sesión
+                    </button>
+                </div>
             </form>
         </LoginLayout>
     );

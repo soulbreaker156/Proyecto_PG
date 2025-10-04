8e13a88821a1ae1e7480594779895ff33cb60aed
soulbreaker156 <soulbreaker0220@gmail.com>
2025-10-04 09:27:21 -0600

    refactor: se quitaron los console.log

--- resources/js/components/FormularioProducto/FormularioProducto.tsx
+++ resources/js/components/FormularioProducto/FormularioProducto.tsx
@@ -1,7 +1,5 @@
 import { Producto, ImagenProducto } from "../Interfaces/interfaceInventario";
 export default function FormularioProducto({productos,imagenes}: {productos?: Producto[]; imagenes?: ImagenProducto[];}){
-console.log(imagenes);
-
 
     return(
         <div className="border-2 border-gray-300 h-[30%] w-[20%] rounded-[5px] p-2">
--- resources/js/pages/EditarProducto/EditarProducto.tsx
+++ resources/js/pages/EditarProducto/EditarProducto.tsx
@@ -4,8 +4,7 @@ import FormularioProducto from "@/components/FormularioProducto/FormularioProduc
 import { Producto,ImagenProducto } from "@/components/Interfaces/interfaceInventario";
 export default function EliminarProducto() {
     const { productos, imagenes } = usePage().props as {productos?:Producto[], imagenes?:ImagenProducto[]};
-console.log(productos);
-console.log(imagenes);
+
     return (
         <SistemaLayout>
            <FormularioProducto productos={productos} imagenes={imagenes} />
